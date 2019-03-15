30705899b45f4826165903f43ec8793b10177eed
Amir Salah <ameer.alahmedy@gmail.com>
2019-03-15 10:53:19 +0200

    better styles fotr home

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -13,66 +13,21 @@ class Home extends Component {
     render() {
         return (
 
-          <div className='home'>
-            {/* <header> */}
+          <div className='Home'>
+          
                <NavBar/>
-              {/* <nav className='memeNav'>  */}
-    {/* <div className='navContainer'> */}
-        {/* <div  className="memeLogo">
-             <NavLink to='/Home/'>Memestock</NavLink>
-        </div>
-        <div className="dropdownMenu">
-            <button>
-                Dropdown
-            </button>
-            <div role='menu' className='dropList'>
-            <input placeholder='Filter'></input>
-                <ul>
-                    <li><NavLink to='/Home/' className='toHome dropdownItem'>Home</NavLink></li>
-                    <li><NavLink to='/popular/' className='sort dropdownItem'>Popular</NavLink></li>
-                    <li><NavLink to='/All/' className='sort dropdownItem'>All</NavLink></li>
-                    <li><NavLink  to='/Hot/'className='sort dropdownItem'>Hot</NavLink></li>
-
-                </ul>
-            </div>
-        </div>
-        <div>
-              <form action='/search' autoComplete='off'>
-                  <input type='search' className='memeSearch' placeholder='Search Memestock' method='get'>
-                  </input>
-              </form>
-        </div>
-        <div className='rightLinksWrapper'>
-              <span className='rightLinks'>
-                <NavLink to='/PM/'>
-                    PM
-                </NavLink>
-              </span>
-              <span className='rightLinks'>
-                <NavLink to='/CreatePost/'>
-                    Create Post
-                </NavLink>
-              </span>
-              <span className='rightLinks'>
-                <a>
-                    Notifications
-                </a>
-              </span>
-        </div> */}
-    {/* </div>    */}
-    {/* </nav>
-            </header> */}
+             
          
               <Route path='/PM/' component={PMs}/>
               <Route path='/CreatePost/'  component={CreatePost}/>
               
               
            
-          {/* // <div className='content'>
-          //   <div className='listing'>
-          //     {/* Threads */}
-         {/*  </div>
-          // </div> */}
+           <div className='content'>
+             <div className='listing'>
+                 Threads
+            </div>
+          </div>
         </div>
           );
       }
