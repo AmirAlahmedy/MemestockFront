f7ac8fd765a05bf98c15b6ce064f15a4cae9f41b
Karim <karim.ibrahim.amin@gmail.com>
2019-03-18 11:32:21 +0200

    Threads updated/not yet finished

--- src/Routes/Thread.js
+++ src/Routes/Thread.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 import './Thread.css';
 
 class Thread extends Component {
+    state = { 
+        username:'GiantSteps_',
+        subreddit:'jazztheory',
+        title:'Modal Harmony Interchange', 
+        content:'Modal Harmony is easier explained than played',
+        comments:[
+            {
+                username:'WreakingHavoc',
+                comment:'I rememeber when I had something to say!'
+            },
+            {
+                username:'CluelessBastard',
+                comment:'Oh this is so relatable'
+            }
+        ],
+        upvotes:0
+    }
     render(){
         return(
            <div>
-
-            <h1>Threads.1</h1>
-            <h1>Threads.2</h1>
-            <h1>Threads.3</h1>
-
+               <div className="threadContainer">
+                <div className="threadSubreddit">r/{this.state.subreddit}</div>
+                <div className="threadUsername">
+                    Posted by u/{this.state.username}
+                </div>
+                <div className="threadTitle"><h2>{this.state.title}</h2></div>
+                <p className="threadContent">{this.state.content}</p>
+                <div className="threadComments">
+                    {this.state.comments.length} Comments
+                </div>
+                <div className="threadUpvotes">{this.state.upvotes}</div>
+               </div>
            </div> 
         );
     }
