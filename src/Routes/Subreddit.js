b92f82ccb5a0ac4e12c6d68ecb7f838bf1c1059f
Karim <karim.ibrahim.amin@gmail.com>
2019-03-18 15:48:17 +0200

    more subreddit page updates

--- src/Routes/Subreddit.js
+++ src/Routes/Subreddit.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Subreddit.css';
 import defImage from '../images/subreddit.png';
+import Listing from '../Components/Listings';
+import Thread from './Thread';
 
 export class Subreddit extends Component {
     state ={
@@ -10,11 +12,19 @@ export class Subreddit extends Component {
   render() {
     return (
       <div className="subredditFixed">
-          <section id="showcase">
-              <img src={defImage} alt="Subreddit Default"/>
-              <h1>r/{this.state.name}</h1>
+        <section id="subredditShowcase">
+            <img src={defImage} alt="Subreddit Default"/>
+            <h1>r/{this.state.name}</h1>
         </section>
-        
+        <div class="subredditContainer">
+            <section id="subredditPageThreads">
+                {/*<Listing/>*/}
+                <Thread/>
+            </section>
+            <aside id="subredditSidebar">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum similique, laudantium omnis, nostrum impedit non, itaque placeat expedita inventore numquam nam velit dolorem in consequuntur eaque quis officiis necessitatibus harum.
+            </aside>
+        </div>
       </div>
     )
   }
