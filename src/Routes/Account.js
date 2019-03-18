06f310fe039a445d66e892262cf3a6cef727673b
youssef998 <47891555+youssef998@users.noreply.github.com>
2019-03-18 12:19:47 +0200

    Some Css Styles

--- src/Routes/Account.js
+++ src/Routes/Account.js
@@ -4,13 +4,13 @@ import './Account.css'
 const Account = () => {
   return (
     <div>
-      <div className="leftlinks">
+      <div >
      {/* <h4 className="left">Account Settings</h4> */}
 
-    <div><a href="#" aria>Change/add Email</a></div>
-  
-    <div><a href="#">Change Password</a></div>
-    
+    <div className="leftlinks"><a href="#" aria->Change/add Email</a></div>
+    <p className="emailParag">The Above Link is not working yet GUYS Stay Tunedd!</p>
+    <div className="leftlinks"><a href="#">Change Password</a></div>
+    <p className="emailParag">Change password is still oUnder Construction. We're in the process of building this functionality. Stay tuned!</p>
         </div>
 
         
