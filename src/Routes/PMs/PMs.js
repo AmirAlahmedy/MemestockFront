86ecc8cb11612b2bf8ffbc14e644974286e0dcbe
Ali Yasser <aliii.hazard1998@gmail.com>
2019-03-24 03:49:43 +0200

    Update PMs.js

--- src/Routes/PMs/PMs.js
+++ src/Routes/PMs/PMs.js
@@ -5,6 +5,7 @@ import Head from  './Head';
 import inbox from  './pages/inbox';
 import sent from  './pages/sent';
 import Aux from '../../Components/HOC/Auxiliary';
+import axios from 'axios';
 
 class PMs extends Component {
    //assume all submitted for now
@@ -12,49 +13,102 @@ class PMs extends Component {
         super(props);
         this.state = {
       
-          To: 'Mohamed',
-          Subject:'Software',
-          Message :'we need to handle our project'
+          To: '',
+          Subject:'',
+          Message :'',
+          
+        
         }
-          this.handleChange = this.handleChange.bind(this);
+          this.handleChange = this.handleChange.bind(this); 
           this.handleSubmit = this.handleSubmit.bind(this);
     };
 
+<<<<<<< HEAD
+
+
+// updating state values with form inputs each time it changes.
+=======
    /**
      * Handles change in PM inputs.
      * @function handleChange
      * @param {event} e - The change of the PM inputs.
      */
+>>>>>>> 8520eb4a8463f8bee7f0f61f01e887d609fc8ec5
     handleChange(event) {
       this.setState({ [event.target.name]: event.target.value });
     }
 
+<<<<<<< HEAD
+//submitting the request
+=======
     /**
      * Handles PM submit.
      * @function handleSubmit
      * @param {event} e - The submission of the PM form.
      */
+>>>>>>> 8520eb4a8463f8bee7f0f61f01e887d609fc8ec5
     handleSubmit(event) { 
           event.preventDefault();
+          const msg ={
+            receiverUsername : this.state.To,
+             subject : this.state.Subject,
+             messageBody : this.state.Message
+             
+          }
+          //checking before posting request if a field isn't filled with any ''
+         
           let checker ="";
 
           if (document.getElementById("1").value===checker)
           {
             alert ("Please provide a username");
-            return false;
+            return ;
           }
           else if (document.getElementById("2").value===checker)
           
             {alert ("Please provide a Subject");
-          return false;}
+          return ;}
           
         else if  (document.getElementById("3").value===checker)
         {
           alert ("Please provide a Message");
-          return false;
+          return ;
         }
-  }
+        console.log(msg);
+        axios.post( 'http://localhost:4000/aasaaa/pm/compose',msg, {
+          headers: {
+              'Content-Type': 'application/json',
     
+          },
+          
+      })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        
+        //in case sucess..
+        alert("your message was sucessfully delievered");
+  
+      })
+      .catch(error => {
+        console.log(error.response)
+        if
+        (error.response.statusText==="Forbidden")
+        {
+          alert("subject is too long");
+        }
+        else if (error.response.statusText==="Not Found")
+        {
+          alert ("User not Found");
+        }
+        else
+        {
+          alert("internal server error");
+        }
+        
+    });
+
+      }
     
   
  render()
@@ -80,6 +134,7 @@ class PMs extends Component {
                           <label>To:</label>
                           <textarea 
                               className="To"
+                              name="To"
                               id="1"
                               placeholder ="(username, or /r/name for that subreddit's moderators)"
                               value={this.state.value} 
@@ -92,6 +147,7 @@ class PMs extends Component {
                             <label>Subject:</label>
                             <textarea 
                                 className="Subject"
+                                name="Subject"
                                 id="2"
                                 value={this.state.value} 
                                 onChange={this.handleChange} 
@@ -103,6 +159,7 @@ class PMs extends Component {
                             <label>Your Message:</label>
                             <textarea 
                                 className="Message"
+                                name="Message"
                                 id="3"
                                 value={this.state.value} 
                                 onChange={this.handleChange} 
