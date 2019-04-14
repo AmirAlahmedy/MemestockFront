3a22f13e6427546c88e5194e06fb35a0922eebeb
nourakamel <47905236+nourakamel@users.noreply.github.com>
2019-04-15 01:03:55 +0200

    comments changes

--- src/Routes/Thread-page/thread-page.js
+++ src/Routes/Thread-page/thread-page.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Thread from '../Thread/Thread';
 import './thread-page.css';
 import { Link, Route, Switch } from 'react-router-dom';
-
+import axios from 'axios';
 
 class ThreadPage extends Component {
    state = {
@@ -10,39 +10,118 @@ class ThreadPage extends Component {
       comments: [
          {
             username: 'WreakingHavoc',
-            comment: 'I rememeber when I had something to say!'
+            comment: 'I rememeber when I had something to say!',
+            Lock: false,
+            Spoiler:false
          },
          {
             username: 'CluelessBastard',
-            comment: 'Oh this is so relatable'
+            comment: 'Oh this is so relatable',
+            Lock: false,
+            Spoiler:false
+         },
+         {
+            username:'Armi',
+            comment:'Awesome',
+            Lock: false,
+            Spoiler:true
          }
       ]
    }
-   render() {
+  
 
+    showReply=()=>{
+
+
+    };
+   
+
+   saveComment(e)  {
+      e.preventDefault();
+      const username=localStorage.getItem("username");
+      //const newComment=this.newComment.value;
+    
+      this.setState({comments:this.state.comments.concat(username,this.newComment.value)});
+      
+
+   };
+
+   render() {
+    
       return (
-         /* <div class="threadPageContainer">
+        /*  <div class="threadPageContainer">
  
-            /* <div className="PageThread">
+             <div className="PageThread">
                 <Thread />
              </div>
           </div>*/
-         <div>
+          
+         <div class="threadPageContainer">
             <div className="PageThread">
                <Thread />
             </div>
-            <div className="threadComment">
-               <div className="commentUser">u/{this.state.comments[0].username} </div>
-               <div className="comment">{this.state.comments[0].comment}</div>
+      
+               <div className="commentContainer">
+           <input  type="text" ref={(input)=>this.newComment=input}  className="textComment" placeholder="write your comment here" ></input>
+           <button className="saveComment" onClick={this.saveComment}>Save</button>
+           </div>
+            <ul>
+                  {
+                    this.state.comments.map(comment => {if(localStorage.getItem("username")===comment.username)
+                    {
+                    if(comment.Spoiler==false)
+                      return ( 
+                        <div className="threadComment">   
+                <div className="commentUser">u/{comment.username} </div>
+               <div className="comment">{comment.comment}</div>
+               <button className="editComment">Edit</button>
+               <button className="deleteComment">Delete</button>
+               <button className="replyComment" onClick={this.showReply}>reply</button>
+               <button className="lockComment">Lock</button>
+               <button className="spoilerComment">Mark as spoiler</button>
+                     </div>
+                     );
+                  else 
+                  return ( 
+                     <div className="threadComment">   
+             <div className="commentUser">u/{comment.username} </div>
+            <div className="spoiler">{comment.comment}</div>
+            <button className="editComment">Edit</button>
+            <button className="deleteComment">Delete</button>
+            <button className="replyComment" onClick={this.showReply}>reply</button>
+            <button className="lockComment">Lock</button>
+            <button className="spoilerComment">Mark as spoiler</button>
+                  </div>
+                  );
+                  }
+                     else 
+                     {
+                     if(comment.Spoiler==false)
+                     return( <div className="threadComment">   
+                     <div className="commentUser">u/{comment.username} </div>
+                    <div className="comment">{comment.comment}</div>
+                    <button className="replyComment" onClick={this.showReply}>reply</button>
+                    </div>
+                    );
+                    else
+                    { return( <div className="threadComment">   
+                    <div className="commentUser">u/{comment.username} </div>
+                   <div className="spoiler">{comment.comment}</div>
+                   <button className="replyComment" onClick={this.showReply}>reply</button>
+                   </div>
+                   );}
+                     }
+                        })
+                  }
+                  
+                </ul>
+                </div>
+                
 
-            </div>
-            <div className="threadComment">
-               <div className="commentUser">u/{this.state.comments[1].username} </div>
-               <div className="comment">{this.state.comments[1].comment}</div>
+      
 
-            </div>
-
-         </div>
+           
+         
       );
 
    }
