b2a416f39385b9acf5df68427796a8782fa6cb82
youssef998 <47891555+youssef998@users.noreply.github.com>
2019-05-02 03:00:17 +0200

    req upd

--- src/Routes/SettingsProf/SettProfile.js
+++ src/Routes/SettingsProf/SettProfile.js
@@ -1,13 +1,52 @@
 import React from 'react'
 import './SettProfile.css'
+import axios from "../../axios-orders";
 
 class SettProfile extends React.Component {
   constructor(props) {
       super(props);
       this.state = { about: '' };
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.handleAboutChange = this.handleAboutChange.bind(this);
   }
+
+  handleAboutChange(e) {
+    this.setState({ about: e.target.value });
+}
+
   handleSubmit(e) {
     e.preventDefault();
+    var data = {
+      About:this.state.about
+  };
+
+  var headers = {
+    'auth': localStorage.getItem("token")
+}
+
+alert(data);
+
+
+
+
+axios.put('me/edit/About', data, { headers: headers })
+.then(res => {
+  console.log(res);
+  if (res.status == 200) {
+      alert("About changed Successfully!");
+  } else if (res.status === 401 || res.status === 404) {
+      alert("About changes Unsuccessful");
+      return Response.json;
+  }
+})
+.catch(error => {
+  alert("Error With About");
+})
+
+
+
+
+
   }
   render() {
   return (
@@ -38,7 +77,7 @@ class SettProfile extends React.Component {
                 </div>
 
                 <div className="ab2">
-                  <textarea placeholder="About (optional)" maxLength="200" rows="5"  id="about"></textarea>
+                  <textarea placeholder="About (optional)" maxLength="200" rows="5"  id="about" onChange={this.handleAboutChange}></textarea>
                   </div>
                   <div class="char">200 Characters</div>
                   <input class="btn btn-primary" type="submit" id="submit" value="Save" />
