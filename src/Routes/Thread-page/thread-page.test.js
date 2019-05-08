a3c7c11a43637b278cdc9e609c9bf0125bd20c15
nourakamel <47905236+nourakamel@users.noreply.github.com>
2019-05-08 03:40:52 +0200

    more tests

--- src/Routes/Thread-page/thread-page.test.js
+++ src/Routes/Thread-page/thread-page.test.js
@@ -2,6 +2,10 @@ import React from "react";
 import { create } from "react-test-renderer";
 import ThreadPage from "./thread-page";
 
+
+configure({adapter: new Adapter()});
+
+
 describe("Subreddit component snapshot", () => {
   test("it matches the snapshot", () => {
     const component = create(<ThreadPage />);
@@ -16,3 +20,54 @@ it("Shows thread page meta", async () => {
     await instance.componentDidMount();
     console.log(instance.state); 
   });
+
+  it("Get user's comments", async () => {
+    const component = create(<ThreadPage />);
+    const instance = component.getInstance();
+    await instance.getCommentsJSX();
+    console.log(instance.state); 
+  });
+
+  it("Edit thread", async () => {
+    const component = create(<ThreadPage />);
+    const instance = component.getInstance();
+    await instance.handleEdit();
+    console.log(instance.state); 
+  });
+
+  it("Edit thread", async () => {
+    const component = create(<ThreadPage />);
+    const instance = component.getInstance();
+    await instance.delPost();
+    console.log(instance.state); 
+  });
+
+  it("Add reply to a comment", async () => {
+    const component = create(<ThreadPage />);
+    const instance = component.getInstance();
+    await instance.replyComment();
+    console.log(instance.state); 
+  });
+
+  it("Add comment to a thread ", async () => {
+    const component = create(<ThreadPage />);
+    const instance = component.getInstance();
+    await instance.addComment();
+    console.log(instance.state); 
+  });
+
+  it("Edit comment of a thread ", async () => {
+    const component = create(<ThreadPage />);
+    const instance = component.getInstance();
+    await instance.goEdit();
+    console.log(instance.state); 
+  });
+
+  it("delete comment of a thread ", async () => {
+    const component = create(<ThreadPage />);
+    const instance = component.getInstance();
+    await instance.deleteComment();
+    console.log(instance.state); 
+  });
+
+  
